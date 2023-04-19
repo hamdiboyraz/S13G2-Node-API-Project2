99d9b4b135c471e6a91efaee17c8b2c813097129
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-19 19:33:18 +0300

    Structure routes and create server

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,13 @@
 // posts için gerekli routerları buraya yazın
+const express = require("express");
+const postsRouter = express.Router();
+
+// Routes
+postsRouter.get("/", (req, res) => {});
+postsRouter.post("/", (req, res) => {});
+postsRouter.get("/:id", (req, res) => {});
+postsRouter.put("/:id", (req, res) => {});
+postsRouter.delete("/:id", (req, res) => {});
+postsRouter.get("/:id/comments", (req, res) => {});
+
+module.exports = postsRouter;
--- api/server.js
+++ api/server.js
@@ -1,3 +1,14 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+const express = require("express");
+const postsRouter = require("./posts/posts-router");
+// INSTANCE OF EXPRESS.JS
+const server = express();
+// GLOBAL MIDDLEWARE
+server.use(express.json());
+
+// API
+server.use("/api/posts", postsRouter);
+// EXPORT
+module.exports = server;
