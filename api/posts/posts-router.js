25fe1cee30b335171eab777db414d3ca4ce1cdef
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-19 22:29:22 +0300

    Add get request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,13 +1,55 @@
 // posts için gerekli routerları buraya yazın
 const express = require("express");
+const postsController = require("./posts-model");
+
+// Create a router object
 const postsRouter = express.Router();
 
-// Routes
-postsRouter.get("/", (req, res) => {});
-postsRouter.post("/", (req, res) => {});
-postsRouter.get("/:id", (req, res) => {});
-postsRouter.put("/:id", (req, res) => {});
-postsRouter.delete("/:id", (req, res) => {});
-postsRouter.get("/:id/comments", (req, res) => {});
+// Define the routes for the posts API
+postsRouter.get("/", async (req, res) => {
+  try {
+    const posts = await postsController.find();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Gönderiler alınamadı" });
+  }
+});
+postsRouter.post("/", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "" });
+  }
+});
+postsRouter.get("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "" });
+  }
+});
+postsRouter.put("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "" });
+  }
+});
+postsRouter.delete("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "" });
+  }
+});
+postsRouter.get("/:id/comments", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "" });
+  }
+});
 
+// Export the router object
 module.exports = postsRouter;
