6c348a293c6d8cc4b5c43d0fa3e8cad75aeec6fb
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-20 01:38:38 +0300

    Add delete request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -88,9 +88,21 @@ postsRouter.put("/:id", async (req, res) => {
 // Delete Post
 postsRouter.delete("/:id", async (req, res) => {
   try {
+    // console.log(req.params);
+    const { id } = req.params;
+    const post = await postsController.findById(id);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    }
+    await postsController.remove(id);
+    // res.status(204).send()
+    // res.status(200).json({ message: "Gönderi başarıyla silindi" });
+    res.status(200).json(post);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "" });
+    res.status(500).json({ message: "Gönderi silinemedi" });
   }
 });
 
